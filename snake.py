4b97839eb9d1208cc165437ac270e1938dcd920f
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-02-28 20:43:01 -0600

    Grid layout is rendered on window

--- snake.py
+++ snake.py
@@ -11,40 +11,58 @@ screen = pygame.display.set_mode([width, height])
 
 green = (0, 128 ,0)
 
-x = width / 2
-y = height / 2
+x = 0
+y = 0 
 
 rectangle = (
     x,
     y,
-    10,
-    10
+    20,
+    20
 )
+
+running = True
 
 colNumber = 0
 rowNumber = 0
 
-while True:
+while running:
 
+    screen.fill((0, 0, 0))
+
+    # USER INPUT
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                
+                x += 10
 
-    pygame.draw.rect(screen, color=green, rect=rectangle)
+            if event.key == pygame.K_LEFT:
+                x -= 10
 
-    # Draw grid
+            if event.key == pygame.K_UP:
+                y += 10
 
-    for col in range(1, int(width / 10)):
+            if event.key == pygame.K_DOWN:
+                y -= 10
 
+    for col in range(1, int(width / 20)):
+
+        colNumber += 20
         pygame.draw.line(screen, (255, 255, 255), [colNumber, 0], [colNumber, height], 1)
 
-        colNumber += 10        
+    for row in range(0, int(height / 20)):
 
-        for row in range(0, int(height / 10)):
-            # pygame.draw.line(screen, (255, 255, 255), [0, rowNumber], [rowNumber, math.sin(math.radians(270)) * height], 1)
+        rowNumber += 20
 
-            rowNumber += 10
+        pygame.draw.line(screen, (255, 255, 255), (0, rowNumber), (0 + width * math.cos(0), rowNumber + width * math.sin(0)), 1)
+
+    pygame.draw.rect(screen, color=green, rect=rectangle)
 
     pygame.display.flip()
 
 pygame.quit()
+sys.exit()
