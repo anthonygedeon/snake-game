0cb8e70f97cc72ace6f78a076c9c99224d8d017f
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-03-02 22:47:56 -0600

    Grid is render

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import pprint
 
 import pygame
 
@@ -9,6 +10,7 @@ width = 500
 height = 400
 
 screen = pygame.display.set_mode([width, height])
+clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -21,49 +23,68 @@ y = 0
 rectangle = (
     x,
     y,
-    20,
-    20
+    19,
+    19
 )
-
-class Grid:
-    
-    grid = []
-
-    def __init__(self, screen):
-        self.screen = screen
-        
-
-    def get_columns(self):
-        return math.floor(self.screen.width / 50)
-
-    def get_rows(self):
-        return math.floor(self.screen.height / 40)
-
-    def get_total_number_of_cells(self):
-        return self.get_columns * self.get_rows
-    
-    def create_grid(self):
-        
-        for row in self.get_rows():
-            for col in self.get_columns():
-                pass
-
-    
 
 class Cell:
     
-    def __init__(self, x, y):
+    def __init__(self, x, y, width):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = width
 
     def create_cell():
         pass
 
+class Grid:
+    
+    def __init__(self, screen):
+        self.screen = screen
+        self.grid = []
+    
+    def get_columns(self):
+        return math.floor(width / 20)
+
+    def get_rows(self):
+        return math.floor(height / 20)
+
+    def get_total_number_of_cells(self):
+        return self.get_columns() * self.get_rows()
+    
+    def create_grid(self):
+        for row in range(0, self.get_rows()):
+            self.grid.append([])
+            for col in range(0, self.get_columns()):
+                self.grid[row].append(Cell(col * 20, row * 20, 20))
+
+        return self.grid
+
+    def draw_grid(self):
+        matrix_grid = self.create_grid()
+
+        for row in range(len(matrix_grid)):
+            for col in range(len(matrix_grid[row])):
+
+                pygame.draw.rect(screen, WHITE, [
+                    matrix_grid[row][col].x, 
+                    matrix_grid[row][col].y, 
+                    matrix_grid[row][col].width, 
+                    matrix_grid[row][col].height], 1)
+
+        return 0
+
+
 running = True
+
+grid = Grid(None)
+
+grid.draw_grid()
 
 while running:
 
-    screen.fill((0, 0, 0))
+    clock.tick(60)
 
     # USER INPUT
     for event in pygame.event.get():
@@ -85,6 +106,12 @@ while running:
                 pass
 
     pygame.draw.rect(screen, color=GREEN, rect=rectangle)
+
+    # pygame.draw.rect(screen, WHITE, [0, 0, 20, 20], 1)
+
+    # pygame.draw.rect(screen, WHITE, [20, 0, 20, 20], 1)
+
+    # pygame.draw.rect(screen, WHITE, [20, 20, 20, 20], 1)
 
     pygame.display.flip()
 
